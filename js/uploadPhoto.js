ebf0f3e484b95ba650706ef352a2205c5cf0cc63
StasRomanov <stasromanov801@gmail.com>
2020-07-13 20:58:56 +0300

    scale

--- js/uploadPhoto.js
+++ js/uploadPhoto.js
@@ -7,8 +7,10 @@
   var uploadPhotoCancel = document.querySelector('#upload-cancel');
   var effectList = document.querySelector('.effects__list');
   var uploadPhotoLevel = document.querySelector('.img-upload__effect-level');
+  var scaleContainer = document.querySelector('.img-upload__scale');
+  var scaleValue = document.querySelector('.scale__control--value');
 
-  var setPhotoFilter = function (evt) {
+  var onEffectListChange = function (evt) {
     if (evt.button === window.data.LEFT_MOUSE_CODE) {
       var target = evt.target;
       if (target.tagName === 'INPUT' && target.id !== 'effect-none') {
@@ -22,20 +24,40 @@
     }
   };
 
+  var onScaleContainerClick = function (evt) {
+    if (evt.button === window.data.LEFT_MOUSE_CODE) {
+      var target = evt.target;
+      if (target.classList.contains('scale__control--bigger') && Number(scaleValue.value.replace('%', '')) < 100) {
+        scaleValue.value = Number(scaleValue.value.replace('%', '')) + 25 + '%';
+        uploadPhotoImage.style.transform = 'scale(' + Number(scaleValue.value.replace('%', '')) / 100 + ')';
+      }
+      if (target.classList.contains('scale__control--smaller') && Number(scaleValue.value.replace('%', '')) > 25) {
+        scaleValue.value = Number(scaleValue.value.replace('%', '')) - 25 + '%';
+        uploadPhotoImage.style.transform = 'scale(' + Number(scaleValue.value.replace('%', '')) / 100 + ')';
+      }
+    }
+  };
+
   uploadPhotoInput.addEventListener('change', function () {
+    uploadPhotoImage.style.transform = 'scale(1)';
+    scaleValue.value = '100%';
     uploadPhotoContainer.classList.remove('hidden');
     document.body.classList.add('modal-open');
     uploadPhotoLevel.classList.add('hidden');
-    effectList.addEventListener('click', setPhotoFilter, false);
+    effectList.addEventListener('click', onEffectListChange, false);
   });
 
   uploadPhotoCancel.addEventListener('click', function (evt) {
     if (evt.button === window.data.LEFT_MOUSE_CODE) {
       uploadPhotoInput.value = '';
+      scaleValue.value = '100%';
       uploadPhotoImage.className = '';
+      uploadPhotoImage.style.transform = 'scale(1)';
       uploadPhotoContainer.classList.add('hidden');
       document.body.classList.remove('modal-open');
-      effectList.removeEventListener('click', setPhotoFilter, false);
+      effectList.removeEventListener('click', onEffectListChange, false);
     }
   });
+
+  scaleContainer.addEventListener('click', onScaleContainerClick, false);
 })();
